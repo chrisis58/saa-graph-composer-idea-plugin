757a6e2dde33dd5e5d70997e48b555071869607f
GitHub Action <action@github.com>
2026-01-20 08:18:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/chrisis58/saagraphcomposerideaplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/chrisis58/saagraphcomposerideaplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.chrisis58.saagraphcomposerideaplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
